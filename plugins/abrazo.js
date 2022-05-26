dea52d727d373a8d5884596245f2a6de0a720311
Misael1287 <105521930+Misael1287@users.noreply.github.com>
2022-05-26 10:11:05 -0500

    Create abrazo.js

---
+++ plugins/abrazo.js
@@ -0,0 +1,15 @@
+import { sticker } from '../lib/sticker.js'
+import fetch from 'node-fetch'
+import MessageType from '@adiwajshing/baileys'
+let handler = async (m, { conn}) => {
+try {
+if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
+if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
+let res = await fetch('https://some-random-api.ml/animu/hug')
+let json = await res.json()
+let { link } = json
+let stiker = await sticker(null, link, `+${m.sender.split('@')[0]} le dio un abrazo a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
+conn.sendFile(m.chat, stiker, null, { asSticker: true })
+} catch (e) { }}
+handler.command = /^(abrazo|hug|consolar)$/i
+export default handler
